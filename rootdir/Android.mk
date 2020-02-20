930c1de8750df993d98977167171fcad0e88af68
Oleksandr Novikov <novikov.firewall@gmail.com>
2020-02-20 18:24:32 +0200

    rootdir\Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,6 +35,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := move_widevine_data.sh
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/move_widevine_data.sh
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
