c889884f2509f990d1255ba7324606e711f8f7ba
f1r3w411 <novikov.firewall@gmail.com>
2020-11-20 14:44:05 +0200

    Move offmode charging daemon away
    Change-Id: Ibdc0d714ae79f8e3bbd5273e3aac3d3dbe67babf

--- msm8956.mk
+++ msm8956.mk
@@ -273,7 +273,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service \
     android.hardware.health@2.0 \
-    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
